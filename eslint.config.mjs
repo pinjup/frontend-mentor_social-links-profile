2a024a35f8b41487d14e5ce61699a7f1f238f238
Juan Pino <pinjup@protonmail.com>
2024-12-26 17:43:08 -0500

    Solucionado

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,16 +1,19 @@
 import { dirname } from "path";
 import { fileURLToPath } from "url";
 import { FlatCompat } from "@eslint/eslintrc";
+import eslintConfigPrettier from "eslint-config-prettier";
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 
 const compat = new FlatCompat({
-  baseDirectory: __dirname,
+    baseDirectory: __dirname
 });
 
 const eslintConfig = [
-  ...compat.extends("next/core-web-vitals", "next/typescript"),
+    ...compat.extends("next/core-web-vitals", "next/typescript")
 ];
 
-export default eslintConfig;
+const config = [eslintConfig, eslintConfigPrettier];
+
+export default config;
